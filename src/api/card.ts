004909c3554e18316ef51e26e686cfd471d6a4ce
ljy6712 <a01045386712@gmail.com>
2023-03-09 13:31:55 +0900

    Fix:필터링 수정

--- src/api/card.ts
+++ src/api/card.ts
@@ -19,6 +19,8 @@ route.use(express.urlencoded({ extended: true }));
 route.get('/', async (req: Request, res: Response) => {
     if (!req.user) return res.status(statusCode.UNAUTHORIZED).json({ message: responseMessage.auth.unauthorized });
     const userId = req.user.user_id;
+    const option = req.query.option;
+    console.log(req);
 
     const pageAsNumber = Number.parseInt(req.query.page as string);
     const sizeAsNumber = Number.parseInt(req.query.size as string);
@@ -28,8 +30,9 @@ route.get('/', async (req: Request, res: Response) => {
 
     let size = 12;
     if (!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0 && sizeAsNumber < 13) size = sizeAsNumber;
+    console.log(page, size);
 
-    cardService.getCards(userId, page, size, (err, data) => {
+    cardService.getCards(userId, page, size, option, (err, data) => {
         if (err) res.status(statusCode.INTERNAL_SERVER_ERROR).send({ err: err, message: responseMessage.card.server_error });
         else res.status(statusCode.OK).send(data);
     });
--- src/api/mail.ts
+++ src/api/mail.ts
@@ -25,27 +25,27 @@ route.post('/', (req: Request, res) => {
     const friend = req.body.friend;
     const expression = req.body.expression;
     const base64Data = req.body.image.replace(/^data:image\/jpeg;base64,/, '');
-    let comment = "";
-    switch (expression){
-        case "happy" :
-            comment = "나 행복했어😊, 무슨 일이 있었냐면.....";
+    let comment = '';
+    switch (expression) {
+        case 'happy':
+            comment = '나 행복했어😊, 무슨 일이 있었냐면.....';
             break;
-        case "sad" :
-            comment = "나 슬펐어😥, 위로해줘.";
+        case 'sad':
+            comment = '나 슬펐어😥, 위로해줘.';
             break;
-        case "angry" :
-            comment = "나 짜증났어😣, 매운거 먹으러 가자!";
+        case 'angry':
+            comment = '나 짜증났어😣, 매운거 먹으러 가자!';
             break;
-        case "surprised" :
-            comment = "나 놀랐어😮, 무슨 일인지 물어봐줘.";
+        case 'surprised':
+            comment = '나 놀랐어😮, 무슨 일인지 물어봐줘.';
             break;
-        case "disgusted" :
-            comment = "나 힘들었어😔, 맛있는거 먹으러 가자!";
+        case 'disgusted':
+            comment = '나 힘들었어😔, 맛있는거 먹으러 가자!';
             break;
-        case "fearful" :
-            comment = "나 무서웠어😨, 같이 있어줘.";
+        case 'fearful':
+            comment = '나 무서웠어😨, 같이 있어줘.';
             break;
-        default :
+        default:
             comment = `${req.user.nickname}님의 감정 레포트를 확인해보세요!`;
     }
     fs.writeFile('expressions_report.jpeg', base64Data, 'base64', function (err) {
@@ -57,7 +57,7 @@ route.post('/', (req: Request, res) => {
                 toEmail: email, // 수신할 이메일
 
                 subject: `[CarpeDiem] ${friend} 회원님! ${req.user.nickname}님께서 감정 리포트를 공유하셨습니다.`, // 메일 제목
-                html:  `<div>${comment}</div>`,
+                html: `<h1>${comment}</h1>`,
                 attachments: [
                     {
                         filename: 'expressions_report.jpeg',
